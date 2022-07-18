7c520b4c3f5f2739335881286123684e92715a21
DELL <DELL@DESKTOP-4CN9CFP>
2022-07-18 12:53:27 +0530

    [Nikhil]:CSS updated

---
+++ src/GH1/sample1.java
@@ -0,0 +1,12 @@
+package GH1;
+
+public class sample1
+{
+   public static void main(String[] args)
+   {
+	 System.out.println("Hello Sachin");
+	 System.out.println("Hello Sachin");
+	 System.out.println("Hello Sachin");
+	 System.out.println("Hello Sachin");
+   }
+}
